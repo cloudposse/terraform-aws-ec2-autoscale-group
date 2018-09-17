3a57cdcd08bfc18acf372f74e0fe29f471668b6d
Andriy Knysh <aknysh@users.noreply.github.com>
2018-09-17 14:52:27 -0400

    Update example (#4)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
 locals {
   userdata = <<USERDATA
     #!/bin/bash
@@ -10,21 +14,44 @@ locals {
   USERDATA
 }
 
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  cidr_block = "10.0.0.0/16"
+}
+
+module "subnets" {
+  source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
+  availability_zones  = ["${data.aws_availability_zones.available.names}"]
+  namespace           = "${var.namespace}"
+  stage               = "${var.stage}"
+  name                = "${var.name}"
+  region              = "${var.region}"
+  vpc_id              = "${module.vpc.vpc_id}"
+  igw_id              = "${module.vpc.igw_id}"
+  cidr_block          = "${module.vpc.vpc_cidr_block}"
+  nat_gateway_enabled = "true"
+}
+
 module "autoscale_group" {
   source = "git::https://github.com/cloudposse/terraform-aws-ec2-autoscale-group.git?ref=master"
 
-  namespace = "eg"
-  stage     = "dev"
-  name      = "test"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
 
-  image_id                    = "ami-08cab282f9979fc7a"
-  instance_type               = "t2.small"
-  security_group_ids          = ["sg-xxxxxxxx"]
-  subnet_ids                  = ["subnet-xxxxxxxx", "subnet-yyyyyyyy", "subnet-zzzzzzzz"]
-  health_check_type           = "EC2"
-  min_size                    = 2
-  max_size                    = 3
-  wait_for_capacity_timeout   = "5m"
+  image_id                    = "${var.image_id}"
+  instance_type               = "${var.instance_type}"
+  security_group_ids          = []
+  subnet_ids                  = ["${module.subnets.public_subnet_ids}"]
+  health_check_type           = "${var.health_check_type}"
+  min_size                    = "${var.min_size}"
+  max_size                    = "${var.max_size}"
+  wait_for_capacity_timeout   = "${var.wait_for_capacity_timeout}"
   associate_public_ip_address = true
   user_data_base64            = "${base64encode(local.userdata)}"
 
@@ -35,6 +62,6 @@ module "autoscale_group" {
 
   # Auto-scaling policies and CloudWatch metric alarms
   autoscaling_policies_enabled           = "true"
-  cpu_utilization_high_threshold_percent = "70"
-  cpu_utilization_low_threshold_percent  = "20"
+  cpu_utilization_high_threshold_percent = "${var.cpu_utilization_high_threshold_percent}"
+  cpu_utilization_low_threshold_percent  = "${var.cpu_utilization_low_threshold_percent}"
 }
