36d79711125c7494cdc9102208b282aa01f7b5ac
Rohit Verma <38323616+rverma-nikiai@users.noreply.github.com>
2019-07-12 17:33:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -240,7 +240,7 @@ variable "min_elb_capacity" {
 
 variable "wait_for_elb_capacity" {
   description = "Setting this will cause Terraform to wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. Takes precedence over `min_elb_capacity` behavior"
-  default     = false
+  default     = 1
 }
 
 variable "protect_from_scale_in" {
