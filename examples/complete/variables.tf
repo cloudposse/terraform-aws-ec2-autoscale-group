741e646e84df2b40819f94ac4128f87e509fc302
Andriy Knysh <aknysh@users.noreply.github.com>
2019-09-25 13:36:17 -0400

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline (#15)
    * Convert to TF 0.12
    
    * Add tests. Add Codefresh test pipeline
    
    * Add tests. Add Codefresh test pipeline
    
    * Add tests. Add Codefresh test pipeline
    
    * Add tests. Add Codefresh test pipeline
    
    * Add tests. Add Codefresh test pipeline
    
    * Add tests. Add Codefresh test pipeline

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,93 +1,64 @@
 variable "namespace" {
-  type        = "string"
-  default     = "eg"
+  type        = string
   description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
 }
 
 variable "stage" {
-  type        = "string"
-  default     = "testing"
+  type        = string
   description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
 }
 
-variable "environment" {
-  type        = "string"
-  default     = ""
-  description = "Environment, e.g. 'testing', 'UAT'"
-}
-
 variable "name" {
-  type        = "string"
-  default     = "test"
+  type        = string
   description = "Solution name, e.g. 'app' or 'cluster'"
 }
 
-variable "delimiter" {
-  type        = "string"
-  default     = "-"
-  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
-}
-
-variable "attributes" {
-  type        = "list"
-  default     = []
-  description = "Additional attributes (e.g. `1`)"
-}
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-  description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
-}
-
 variable "region" {
-  type        = "string"
-  default     = "us-west-2"
+  type        = string
   description = "AWS Region"
 }
 
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
 variable "image_id" {
-  type        = "string"
-  default     = "ami-08cab282f9979fc7a"
+  type        = string
   description = "The EC2 image ID to launch"
 }
 
 variable "instance_type" {
-  type        = "string"
-  default     = "t2.small"
+  type        = string
   description = "Instance type to launch"
 }
 
 variable "health_check_type" {
-  type        = "string"
-  default     = "EC2"
+  type        = string
   description = "Controls how health checking is done. Valid values are `EC2` or `ELB`"
 }
 
 variable "wait_for_capacity_timeout" {
-  type        = "string"
-  default     = "10m"
+  type        = string
   description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. (See also Waiting for Capacity below.) Setting this to '0' causes Terraform to skip all Capacity Waiting behavior"
 }
 
 variable "max_size" {
-  default     = 2
+  type        = number
   description = "The maximum size of the autoscale group"
 }
 
 variable "min_size" {
-  default     = 3
+  type        = number
   description = "The minimum size of the autoscale group"
 }
 
 variable "cpu_utilization_high_threshold_percent" {
-  type        = "string"
-  default     = "80"
+  type        = number
   description = "CPU utilization high threshold"
 }
 
 variable "cpu_utilization_low_threshold_percent" {
-  type        = "string"
-  default     = "20"
-  description = "CPU utilization loq threshold"
+  type        = number
+  description = "CPU utilization low threshold"
 }
