b75bee2f6da3b321a8a0d43c34172d51fa910da1
Maxim Mironenko <maxim@cloudposse.com>
2020-12-21 17:28:19 +0700

    Terraform 0.14 upgrade

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,32 +3,28 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.0"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
+  source     = "cloudposse/vpc/aws"
+  version    = "0.18.1"
   cidr_block = "172.16.0.0/16"
+
+  context = module.this.context
 }
 
 module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.18.1"
+  source               = "cloudposse/dynamic-subnets/aws"
+  version              = "0.33.0"
   availability_zones   = var.availability_zones
-  namespace            = var.namespace
-  stage                = var.stage
-  name                 = var.name
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
   nat_gateway_enabled  = false
   nat_instance_enabled = false
+
+  context = module.this.context
 }
 
 module "autoscale_group" {
   source = "../../"
-
-  namespace = var.namespace
-  stage     = var.stage
-  name      = var.name
 
   image_id                    = var.image_id
   instance_type               = var.instance_type
@@ -51,6 +47,8 @@ module "autoscale_group" {
   autoscaling_policies_enabled           = true
   cpu_utilization_high_threshold_percent = var.cpu_utilization_high_threshold_percent
   cpu_utilization_low_threshold_percent  = var.cpu_utilization_low_threshold_percent
+
+  context = module.this.context
 }
 
 # https://www.terraform.io/docs/configuration/expressions.html#string-literals
