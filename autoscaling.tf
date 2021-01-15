7ec246fbdcd2687b9a1f53a56349bf2ad5a185db
PePe Amengual <jose.amengual@gmail.com>
2021-01-15 15:30:21 -0800

    adding custom alarms

--- autoscaling.tf
+++ autoscaling.tf
@@ -61,3 +61,62 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   alarm_description = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} seconds"
   alarm_actions     = [join("", aws_autoscaling_policy.scale_down.*.arn)]
 }
+
+resource "aws_cloudwatch_metric_alarm" "custom_alarms" {
+  for_each                  = module.this.enabled ? var.custom_alarms : {}
+  alarm_name                = format("%s%s", "${module.this.id}${module.this.delimiter}", each.value.alarm_name)
+  comparison_operator       = lookup(each.value, "comparison_operator", null)
+  evaluation_periods        = lookup(each.value, "evaluation_periods", null)
+  metric_name               = lookup(each.value, "metric_name", null)
+  namespace                 = lookup(each.value, "namespace", null)
+  period                    = lookup(each.value, "period", null)
+  statistic                 = lookup(each.value, "statistic", null)
+  threshold                 = lookup(each.value, "threshold", null)
+  treat_missing_data        = lookup(each.value, "treat_missing_data", null)
+  ok_actions                = lookup(each.value, "ok_actions", null)
+  insufficient_data_actions = lookup(each.value, "insufficient_data_actions", null)
+  dimensions = {
+    lookup(each.value, "dimensions_name", null) = lookup(each.value, "dimensions_target", null)
+  }
+
+  alarm_description = lookup(each.value, "alarm_description", null)
+  alarm_actions     = [join("", aws_autoscaling_policy.scale_down.*.arn)]
+}
+
+variable "custom_alarms" {
+  type = map(object({
+    alarm_name                = string
+    comparison_operator       = string
+    evaluation_periods        = string
+    metric_name               = string
+    namespace                 = string
+    period                    = string
+    statistic                 = string
+    threshold                 = string
+    treat_missing_data        = string
+    ok_actions                = list(string)
+    insufficient_data_actions = list(string)
+    dimensions_name           = string
+    dimensions_target         = string
+    alarm_description         = string
+    alarm_actions             = list(string)
+  }))
+  default = {
+    alarm_name                = null
+    comparison_operator       = null
+    evaluation_periods        = null
+    metric_name               = null
+    namespace                 = null
+    period                    = null
+    statistic                 = null
+    threshold                 = null
+    treat_missing_data        = null
+    ok_actions                = []
+    insufficient_data_actions = []
+    dimensions_name           = null
+    dimensions_target         = null
+    alarm_description         = null
+    alarm_actions             = []
+  }
+  description = "List of custom CloudWatch alarms configurations"
+}
