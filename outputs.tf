af35c2f2de7853beb1a92e06a49e7541cd66d5ab
Scott Doyland <scott.doyland@hogarth.com>
2023-04-11 18:27:34 +0100

    update tags to tag

--- outputs.tf
+++ outputs.tf
@@ -20,7 +20,7 @@ output "autoscaling_group_name" {
 
 output "autoscaling_group_tags" {
   description = "A set of maps containing tags associated with the AutoScaling Group"
-  value       = module.this.enabled ? aws_autoscaling_group.default[0].tags : []
+  value       = module.this.enabled ? aws_autoscaling_group.default[0].tag : []
 }
 
 output "autoscaling_group_arn" {
