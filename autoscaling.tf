47d4ed2a65f9b5c69398f0518212d9333acb4995
Max Lobur <max_lobur@outlook.com>
2023-05-30 19:29:11 +0300

    Support AWS Provider V5

--- autoscaling.tf
+++ autoscaling.tf
@@ -10,7 +10,7 @@ resource "aws_autoscaling_policy" "scale_up" {
   adjustment_type        = var.scale_up_adjustment_type
   policy_type            = var.scale_up_policy_type
   cooldown               = var.scale_up_cooldown_seconds
-  autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
+  autoscaling_group_name = one(aws_autoscaling_group.default[*].name)
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
@@ -20,7 +20,7 @@ resource "aws_autoscaling_policy" "scale_down" {
   adjustment_type        = var.scale_down_adjustment_type
   policy_type            = var.scale_down_policy_type
   cooldown               = var.scale_down_cooldown_seconds
-  autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
+  autoscaling_group_name = one(aws_autoscaling_group.default[*].name)
 }
 
 locals {
@@ -36,9 +36,9 @@ locals {
       extended_statistic        = null
       threshold                 = var.cpu_utilization_high_threshold_percent
       dimensions_name           = "AutoScalingGroupName"
-      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
+      dimensions_target         = one(aws_autoscaling_group.default[*].name)
       alarm_description         = "Scale up if CPU utilization is above ${var.cpu_utilization_high_threshold_percent} for ${var.cpu_utilization_high_period_seconds} * ${var.cpu_utilization_high_evaluation_periods} seconds"
-      alarm_actions             = [join("", aws_autoscaling_policy.scale_up.*.arn)]
+      alarm_actions             = [one(aws_autoscaling_policy.scale_up[*].arn)]
       treat_missing_data        = "missing"
       ok_actions                = []
       insufficient_data_actions = []
@@ -54,9 +54,9 @@ locals {
       extended_statistic        = null
       threshold                 = var.cpu_utilization_low_threshold_percent
       dimensions_name           = "AutoScalingGroupName"
-      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
+      dimensions_target         = one(aws_autoscaling_group.default[*].name)
       alarm_description         = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} * ${var.cpu_utilization_high_evaluation_periods} seconds"
-      alarm_actions             = [join("", aws_autoscaling_policy.scale_down.*.arn)]
+      alarm_actions             = [one(aws_autoscaling_policy.scale_down[*].arn)]
       treat_missing_data        = "missing"
       ok_actions                = []
       insufficient_data_actions = []
