6cc3ff5c7e8c0a54f61c60b671da084ba41b21ea
Kamil Grabowski <kamil.grabowski@gmail.com>
2025-10-16 22:59:29 +0200

    calculate cpu_count

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ data "aws_subnet" "this" {
   id       = each.value
 }
 
+data "aws_ec2_instance_type" "default" {
+  count = (
+    can(var.cpu_options.threads_per_core) && var.cpu_options.core_count == null ? 1 : 0
+  )
+
+  instance_type = var.instance_type
+}
+
 resource "aws_launch_template" "default" {
   count = module.this.enabled ? 1 : 0
 
@@ -90,8 +98,15 @@ resource "aws_launch_template" "default" {
   dynamic "cpu_options" {
     for_each = var.cpu_options != null ? [var.cpu_options] : []
     content {
-      amd_sev_snp      = lookup(cpu_options.value, "amd_sev_snp_enabled", null) != null ? (lookup(cpu_options.value, "amd_sev_snp_enabled") ? "enabled" : "disabled") : null
-      core_count       = lookup(cpu_options.value, "core_count", null)
+      amd_sev_snp      = can(cpu_options.value.amd_sev_snp_enabled) ? (cpu_options.value.amd_sev_snp_enabled ? "enabled" : "disabled") : null
+
+      # if threads_per_core is set and core_count is not set, use the default cores from the instance type
+      core_count       = (
+        cpu_options.value.core_count != null ? cpu_options.value.core_count : (
+          cpu_options.value.threads_per_core == null ? null : one(data.aws_ec2_instance_type.default[*].default_cores)
+        )
+      )
+
       threads_per_core = lookup(cpu_options.value, "threads_per_core", null)
     }
   }
